4ac9da116a298f8ea0eb016ea6cc435e80d38922
ankit-yadav90 <ankityadav73699@gmail.com>
2025-03-05 14:27:56 +0530

    Updete opertors

--- first.js
+++ first.js
@@ -2,7 +2,28 @@
 let a = 5;
 let b = 2;
 
-console.log("a + b" , a + b);
-console.log("a - b" , a - b);
-console.log("a * b" , a * b);
-console.log("a / b" , a / b);
+console.log("a = ",a , " & b =", b);
+console.log("a + b =" , a + b);
+console.log("a - b =" , a - b);
+console.log("a * b =" , a * b);
+console.log("a / b =" , a / b);
+console.log("a % b =" , a % b);
+console.log("a ** b =" , a ** b); 
+
+
+// Unary Operators
+
+a++;
+console.log(a);
+
+a--;
+console.log(a);
+
+//post Operators
+
+console.log("a++ = ", a++);
+console.log("a = ", a);
+
+//pre Operators
+
+console.log("++a = ", ++a);
